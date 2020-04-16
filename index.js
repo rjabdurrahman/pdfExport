5c34cc986d287951b607d6e070f72cbd1bd9bfe6
rjabdurrahman <rjrahman@DESKTOP-ADG9250.localdomain>
2020-04-16 11:11:25 +0600

    Solved: Getting exact Port No

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.get('/', (req, res) => {
     res.send('HOMEPAGE');
 });
 
-const server = app.listen(process.env.PORT || 3000, () => console.log(`Listenin on Port ${server._connectionKey}`));
+const server = app.listen(process.env.PORT || 3000, () => console.log(`Listenin on Port ${server.address().port}`));
