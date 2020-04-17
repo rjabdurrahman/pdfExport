dd04a758a10fe38bc8f2a752173d2e3d6ece778c
aamamun8130 <md.abdullah8130@gmail.com>
2020-04-17 11:38:37 +0600

    Trying: Update

--- assets/page2.js
+++ assets/page2.js
@@ -1,34 +1,34 @@
 let data = {
     enfants: {
         a: {
-            nom,
-            prenom,
-            date_de_naissance,
-            demande_moderation,
+            nom:'',
+            prenom:'',
+            date_de_naissance:'',
+            demande_moderation:'',
             specification_formation
         },
         b: {
-            nom,
-            prenom,
-            date_de_naissance,
+            nom:'',
+            prenom:'',
+            date_de_naissance:'',
             demande_moderation
         },
         c: {
-            nom,
-            prenom,
-            date_de_naissance,
+            nom:'',
+            prenom:'',
+            date_de_naissance:'',
             demande_moderation
         }
     },
     demande_cim: {
-        is_demande_cim,
-        nom_prenom,
-        montant_mensuel_allocation,
+        is_demande_cim:'',
+        nom_prenom:'',
+        montant_mensuel_allocation:'',
         moyens_subsistance
     },
     demande_bonification_impot: {
-        is_demande_bonification_impot,
-        nom_prenom,
+        is_demande_bonification_impot:'',
+        nom_prenom:'',
         date_de_naissance
     }
 }
--- index.js
+++ index.js
@@ -12,4 +12,11 @@ app.get('/', (req, res) => {
      console.log("new data inserted");
 });
 
+app.get('/update', (req, res) => {
+    res.send('Update Page');
+     var query = { _id: '5e99248c1a01931630d48ad5' };
+     Thing.findOneAndUpdate(query, { name: 'jason bourne' },{ upsert:true});
+     console.log("new data updated");
+});
+
 const server = app.listen(process.env.PORT || 3000, () => console.log(`Listenin on Port ${server.address().port}`));
