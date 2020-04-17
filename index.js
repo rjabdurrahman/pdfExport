06c2f223bbe678020bf22c05109ef0e29233f51e
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-04-17 11:17:55 +0600

    Added: Post route and data show

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const express = require('express');
 const app = express();
 const connectDB = require('./config');
 connectDB();
-const Thing = require('./Models/Client');
+const Client = require('./Models/Client');
 let data = require('./assets/page1');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('HOMEPAGE');
-    var thing = new Thing(data);
-    thing.save();
+    var clients = new Client(data);
+    clients.save();
     console.log("new data inserted");
 });
+
+app.post('/update/identification', (req, res) => {
+    let idf = req.body;
+    res.send(`Nom is: ${idf.name}, Prenom is: ${idf.prenom}`);
+})
 
 const server = app.listen(process.env.PORT || 3000, () => console.log(`Listenin on Port ${server.address().port}`));
