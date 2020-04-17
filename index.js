d4c5f14e0f1d6f0f388ec111b47c2ed1bf73bf30
aamamun8130 <md.abdullah8130@gmail.com>
2020-04-17 13:36:13 +0600

    Done and modify: infant update and page2 modification

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 const connectDB = require('./config');
 connectDB();
 const Client = require('./Models/Client');
-let data = require('./assets/page1');
+let data1 = require('./assets/page1');
+let data2 = require('./assets/page2');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('HOMEPAGE');
-    var clients = new Client(data);
+    var clients = new Client(data1);
     clients.save();
     console.log("new data inserted");
 });
@@ -17,7 +18,7 @@ app.get('/', (req, res) => {
 app.get('/update', (req, res) => {
     // res.send('Update Page');
     let ObjectID = require('mongodb').ObjectID;
-    Client.findOneAndUpdate({ "_id": ObjectID("5e9944aef84f890dfc896749")}, { "name": "jason bourne" }, { upsert: true }, (err, result) => {
+    Client.findOneAndUpdate({ "_id": ObjectID("5e994ea097f4e10c5cd74b6d")}, data2, { upsert: true}, (err, result) => {
         if(err) res.send(err)
         else res.send(result)
     });
