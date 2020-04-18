d189a2f368601e4fece4fc12eaeb0ee680660555
aamamun8130 <md.abdullah8130@gmail.com>
2020-04-18 12:56:12 +0600

    Done: page 3 and page 4 data object making

---
+++ assets/page3.js
@@ -0,0 +1,49 @@
+let data3 = {
+    etat_civil: {
+        is_celibataire: '',
+        is_marie_e: '',
+        is_divorce_e: '',
+        is_veuf_veuve: '',
+        is_depuis_le: '',
+        separe_e: {
+            is_dispense_legale: '',
+            is_jugement_separation: '',
+            is_dispense_autorite_judiciaire: '',
+            is_le: ''
+        },
+        non_residents: {
+            contribuable: {
+                nom_prenom: '',
+                date_de_naissance: '',
+                numero: '',
+                rue: '',
+                code_postal: '',
+                localite: ''
+            },
+            conjoint_partenaire: {
+                nom_prenom: '',
+                date_de_naissance: '',
+                numero: '',
+                rue: '',
+                code_postal: '',
+                localite: ''
+            }
+        },
+        assimilation: {
+            a: {
+                revenus_mondiaux: ''
+            },
+            b: {
+                revenus_nets_annuels: ''
+            },
+            c: {
+                revenus_professionels: ''
+            },
+            calculation: {
+                total_revenus_exoneres: '',
+                total_revenus_non_exonores_et_exoneres: '',
+                pourcentage: ''
+            }
+        }
+    }
+}
---
+++ assets/page4.js
@@ -0,0 +1,46 @@
+let data4 = {
+    options: {
+        epoux_contribuale: {
+            is_imposition_collective: ''
+        },
+        partenaires: {
+            is_imposition_collective: '',
+            is_declaration_partenariat: '',
+            is_annexe: '',
+            is_deja_presente: ''
+        },
+        imposition_individuelle: {
+            is_confirmation_choix: '',
+            is_courrier: '',
+            is_myguichet_lu: '',
+            is_annee_imposition: '',
+            is_imposition_collective: '',
+            is_imposition_individuelle_pure: '',
+            is_imposition_individuelle_reallocation: ''
+        },
+        informations_complementaires: {
+            contribuable: {
+                date_de_naissance: '',
+                numero_dossier_individuel: '',
+                titulaire_du_compte: '',
+                code_iban: '',
+                swift_bic: '',
+                taux_avances_communes: '',
+                taux_revenu_imposable: ''
+            },
+            conjoint_partenaire: {
+                date_de_naissance: '',
+                numero_dossier_individuel: '',
+                titulaire_du_compte: '',
+                code_iban: '',
+                swift_bic: '',
+                taux_avances_communes: '',
+                taux_revenu_imposable: ''
+            }
+        },
+        signature: {
+            lieu: '',
+            date: ''
+        }
+    }
+}
