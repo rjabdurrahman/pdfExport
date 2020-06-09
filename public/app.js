3d810e26deeb2fb1d37c142efa7f9bae5ab9a628
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-06-09 17:03:31 +0600

    Fixed: Data load in info page

--- public/app.js
+++ public/app.js
@@ -185,19 +185,7 @@ app.component('page20', {
 });
 
 app.controller('NavCtrl', function ($scope, $http) {
-    let clientId = location.href.split('id=')[1];
-    $scope.client = {};
-    $http({
-        method: 'GET',
-        url: `/api/client/${clientId}`
-    }).then(function (res) {
-        $scope.client = res.data;
-        console.log(res.data)
-        $scope.$applyAsync();
-    }).catch(function (err) {
-        console.log(err);
-    })
-    console.log($scope.client);
+    console.log('Nav page');
 });
 
 app.controller('ClientsListControler', function ($scope, $http) { });
