6f8a73d724e08b9f1c0fe873e16321c0d252e2c8
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-06-10 19:12:10 +0600

    Added: Html compilor to dir

---
+++ dev/html-compile.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+const config = require('./html-config.json');
+let importsRegx = /{{<.+}}$/gm
+let html = '';
+let dir = config.rootDir;
+fs.readFile(dir + 'info1.html', 'UTF-8', (err, text) => {
+    if (err) console.log(err.message)
+    html = text
+    for (e of text.match(importsRegx)) {
+        let text = fs.readFileSync(dir + e.replace('{{<', '').replace('}}', ''), { encoding: 'utf8' });
+        html = html.replace(e, text);
+    }
+    fs.writeFile(config.outDir + 'info1.html', html, err => {
+        if (err) console.log(err.message)
+        else console.log('HTML Compiled');
+    });
+});
