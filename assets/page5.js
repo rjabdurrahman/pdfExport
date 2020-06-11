08a7b7962bf2aec3a6a98c8c80af74c2591b6e80
aamamun8130 <md.abdullah8130@gmail.com>
2020-06-11 08:50:15 +0600

    Added: page 4 attribute and value adding

--- assets/page5.js
+++ assets/page5.js
@@ -5,82 +5,82 @@ module.exports = function(info){
             drter_ben_com: {
                 rev_non_expo: {
                     contribuable: {
-                        benefice_entreprise: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses_forfaitaires: info['5d'],
-                        depenses_annexe: info['5d'],
-                        benefice_cessation: info['5d'],
-                        exoneration: info['5d']
+                        benefice_entreprise: info['5d1'],
+                        parts_benefice: info['5d5'],
+                        recettes: info['5d9'],
+                        depenses_forfaitaires: info['5d13'],
+                        depenses_annexe: info['5d17'],
+                        benefice_cessation: info['5d21'],
+                        exoneration: info['5d30']
                     },
                     conjoint_partenaire: {
-                        benefice_entreprise: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses_forfaitaires: info['5d'],
-                        depenses_annexe: info['5d'],
-                        benefice_cessation: info['5d'],
-                        exoneration: info['5d']
+                        benefice_entreprise: info['5d2'],
+                        parts_benefice: info['5d6'],
+                        recettes: info['5d10'],
+                        depenses_forfaitaires: info['5d14'],
+                        depenses_annexe: info['5d18'],
+                        benefice_cessation: info['5d22'],
+                        exoneration: info['5d31']
                     }
                 },
                 rev_expo: {
                     contribuable: {
-                        benefice_entreprise: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses_forfaitaires: info['5d'],
-                        depenses_annexe: info['5d'],
-                        benefice_cessation: info['5d']
+                        benefice_entreprise: info['5d3'],
+                        parts_benefice: info['5d7'],
+                        recettes: info['5d11'],
+                        depenses_forfaitaires: info['5d15'],
+                        depenses_annexe: info['5d19'],
+                        benefice_cessation: info['5d23']
                     },
                     conjoint_partenaire: {
-                        benefice_entreprise: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses_forfaitaires: info['5d'],
-                        depenses_annexe: info['5d'],
-                        benefice_cessation: info['5d']
+                        benefice_entreprise: info['5d4'],
+                        parts_benefice: info['5d8'],
+                        recettes: info['5d12'],
+                        depenses_forfaitaires: info['5d16'],
+                        depenses_annexe: info['5d20'],
+                        benefice_cessation: info['5d24']
                     }
                 },
-                etat_partie: info['5d']
+                etat_partie: info['5d33']
             },
             deter_ben_agri: {
                 rev_non_expo: {
                     contribuable: {
-                        benefice_agricole: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses: info['5d'],
-                        benefice_cessation: info['5d'],
-                        investissements: info['5d'],
-                        exoneration: info['5d']
+                        benefice_agricole: info['5d38'],
+                        parts_benefice: info['5d42'],
+                        recettes: info['5d46'],
+                        depenses: info['5d50'],
+                        benefice_cessation: info['5d54'],
+                        investissements: info['5d63'],
+                        exoneration: info['5d66']
                     },
                     conjoint_partenaire: {
-                        benefice_agricole: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses: info['5d'],
-                        benefice_cessation: info['5d'],
-                        investissements: info['5d'],
-                        exoneration: info['5d']
+                        benefice_agricole: info['5d39'],
+                        parts_benefice: info['5d43'],
+                        recettes: info['5d47'],
+                        depenses: info['5d51'],
+                        benefice_cessation: info['5d55'],
+                        investissements: info['5d64'],
+                        exoneration: info['5d67']
                     }
                 },
                 rev_expo: {
                     contribuable: {
-                        benefice_agricole: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses: info['5d'],
-                        benefice_cessation: info['5d']
+                        benefice_agricole: info['5d40'],
+                        parts_benefice: info['5d44'],
+                        recettes: info['5d48'],
+                        depenses: info['5d52'],
+                        benefice_cessation: info['5d56']
                     },
                     conjoint_partenaire: {
-                        benefice_agricole: info['5d'],
-                        parts_benefice: info['5d'],
-                        recettes: info['5d'],
-                        depenses: info['5d'],
-                        benefice_cessation: info['5d']
+                        benefice_agricole: info['5d41'],
+                        parts_benefice: info['5d45'],
+                        recettes: info['5d49'],
+                        depenses: info['5d53'],
+                        benefice_cessation: info['5d57']
                     }
                 },
-                etat_partie: info['5d']
+                etat_partie: info['5d69']
             }
         }
     }
