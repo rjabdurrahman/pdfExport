b578758807cc620796e9617fc4977ac8102c155e
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-06-18 12:45:07 +0600

    Trying: to done icon

--- public/controllers/info.js
+++ public/controllers/info.js
@@ -1,3 +1,13 @@
+function checkDone() {
+    $('.a01, .a02').each(function (i) {
+        $('#' + this.id + ' input:text').each(function (j) {
+            if (this.value) {
+                // $(menuClass).addClass('done')
+                console.dir($('.left-nav'))
+            }
+        })
+    })
+}
 app.controller('InfoCtrl', function ($scope, $http) {
     $scope.name = "Some data";
     let clientId = location.href.split('id=')[1];
@@ -9,6 +19,7 @@ app.controller('InfoCtrl', function ($scope, $http) {
         $scope.client = res.data;
         console.log(res.data)
         $scope.$applyAsync();
+        setTimeout(checkDone, 500);
     }).catch(function (err) {
         console.log(err);
     })
