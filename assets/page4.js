73184eca0af064f0b13817869afbf380f984abdf
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-06-23 16:02:54 +0600

    Done: Page 4 checkboxs view

--- assets/page4.js
+++ assets/page4.js
@@ -3,22 +3,22 @@ module.exports = function(info){
     return {
         options: {
             epoux_contribuable: {
-                is_imposition_collective: info['4d']
+                is_imposition_collective: info['4d1'] ? info['4d1'] : 'Off'
             },
             partenaires: {
-                is_imposition_collective: info['4d'],
+                is_imposition_collective: info['4d2'] ? info['4d2'] : 'Off',
                 is_date_declaration_partenariat: info['4d3'],
-                is_annexe: info['4d'],
-                is_deja_presente: info['4d']
+                is_annexe: info['4d4'] ? info['4d4'] : 'Off',
+                is_deja_presente: info['4d5'] ? info['4d5'] : 'Off'
             },
             imposition_individuelle: {
-                is_confirmation_choix: info['4d'],
-                is_courrier: info['4d'],
-                is_myguichet_lu: info['4d'],
-                is_annee_imposition: info['4d'],
-                is_imposition_collective: info['4d'],
-                is_imposition_individuelle_pure: info['4d'],
-                is_imposition_individuelle_reallocation: info['4d']
+                is_confirmation_choix: info['4d6'] ? info['4d6'] : 'Off',
+                is_courrier: info['4d7'] ? info['4d7'] : 'Off',
+                is_myguichet_lu: info['4d8'] ? info['4d8'] : 'Off',
+                is_annee_imposition: info['4d9'] ? info['4d9'] : 'Off',
+                is_imposition_collective: info['4d10'] ? info['4d10'] : 'Off',
+                is_imposition_individuelle_pure: info['4d11'] ? info['4d11'] : 'Off',
+                is_imposition_individuelle_reallocation: info['4d12'] ? info['4d12'] : 'Off'
             },
             informations_complementaires: {
                 contribuable: {
