726ce5953c09959298fa0defa70d00f904a886c9
aamamun8130 <md.abdullah8130@gmail.com>
2020-06-25 16:31:20 +0600

    Done: page 7 update form data without checkbox

--- assets/page7.js
+++ assets/page7.js
@@ -10,120 +10,120 @@ module.exports = function (info) {
                     autres_texte: info['7d713'],
                     salaire_brut: info['7d722'],
                     a_deduire: {
-                        salaires: info['7d7'],
-                        supplements: info['7d7'],
-                        autres_exemptions: info['7d7'],
-                        autres_exemptions_texte: info['7d7'],
-                        frais_obtention: info['7d7'],
-                        frais_de_deplacement: info['7d7'],
-                        design_lieu_travail: info['7d7']
+                        salaires: info['7d730'],
+                        supplements: info['7d734'],
+                        autres_exemptions: info['7d738'],
+                        autres_exemptions_texte: info['7d742'],
+                        frais_obtention: info['7d743'],
+                        frais_de_deplacement: info['7d747'],
+                        design_lieu_travail: info['7d751']
                     }
                 },
                 conjoint_partenaire: {
-                    premier_contrat: info['7d7'],
-                    deuxieme_contrat: info['7d7'],
-                    prestations: info['7d7'],
-                    autres: info['7d7'],
-                    autres_texte: info['7d7'],
-                    salaire_brut: info['7d7'],
+                    premier_contrat: info['7d702'],
+                    deuxieme_contrat: info['7d706'],
+                    prestations: info['7d710'],
+                    autres: info['7d715'],
+                    autres_texte: info['7d713'],
+                    salaire_brut: info['7d723'],
                     a_deduire: {
-                        salaires: info['7d7'],
-                        supplements: info['7d7'],
-                        autres_exemptions: info['7d7'],
-                        autres_exemptions_texte: info['7d7'],
-                        frais_obtention: info['7d7'],
-                        frais_de_deplacement: info['7d7'],
-                        design_lieu_travail: info['7d7']
+                        salaires: info['7d731'],
+                        supplements: info['7d735'],
+                        autres_exemptions: info['7d739'],
+                        autres_exemptions_texte: info['7d742'],
+                        frais_obtention: info['7d744'],
+                        frais_de_deplacement: info['7d748'],
+                        design_lieu_travail: info['7d752']
                     }
                 }
             },
             rev_exo: {
                 contribuable: {
-                    premier_contrat: info['7d7'],
-                    deuxieme_contrat: info['7d7'],
-                    prestations: info['7d7'],
-                    autres: info['7d7'],
-                    autres_texte: info['7d7'],
-                    salaire_brut: info['7d7'],
+                    premier_contrat: info['7d703'],
+                    deuxieme_contrat: info['7d707'],
+                    prestations: info['7d711'],
+                    autres: info['7d716'],
+                    autres_texte: info['7d713'],
+                    salaire_brut: info['7d724'],
                     a_deduire: {
-                        salaires: info['7d7'],
-                        supplements: info['7d7'],
-                        autres_exemptions: info['7d7'],
-                        autres_exemptions_texte: info['7d7'],
-                        frais_obtention: info['7d7'],
-                        frais_de_deplacement: info['7d7'],
-                        design_lieu_travail: info['7d7']
+                        salaires: info['7d732'],
+                        supplements: info['7d736'],
+                        autres_exemptions: info['7d740'],
+                        autres_exemptions_texte: info['7d742'],
+                        frais_obtention: info['7d745'],
+                        frais_de_deplacement: info['7d749'],
+                        design_lieu_travail: info['7d753']
                     }
                 },
                 conjoint_partenaire: {
-                    premier_contrat: info['7d7'],
-                    deuxieme_contrat: info['7d7'],
-                    prestations: info['7d7'],
-                    autres: info['7d7'],
-                    autres_texte: info['7d7'],
-                    salaire_brut: info['7d7'],
+                    premier_contrat: info['7d704'],
+                    deuxieme_contrat: info['7d708'],
+                    prestations: info['7d712'],
+                    autres: info['7d717'],
+                    autres_texte: info['7d713'],
+                    salaire_brut: info['7d725'],
                     a_deduire: {
-                        salaires: info['7d7'],
-                        supplements: info['7d7'],
-                        autres_exemptions: info['7d7'],
-                        autres_exemptions_texte: info['7d7'],
-                        frais_obtention: info['7d7'],
-                        frais_de_deplacement: info['7d7'],
-                        design_lieu_travail: info['7d7']
-                    }
-                }
-            }
-        },
-        plusieurs_lieux: {
-            lieu1: {
-                contribuable: {
-                    localite: info['7d7'],
-                    periode: {
-                        du: info['7d7'],
-                        au: info['7d7']
-                    },
-                    frequence: {
-                        jour: info['7d7'],
-                        is_par_semaine: info['7d7'],
-                        is_par_mois: info['7d7']
-                    }
-                },
-                conjoint_partenaire: {
-                    localite: info['7d7'],
-                    periode: {
-                        du: info['7d7'],
-                        au: info['7d7']
-                    },
-                    frequence: {
-                        jour: info['7d7'],
-                        is_par_semaine: info['7d7'],
-                        is_par_mois: info['7d7']
+                        salaires: info['7d733'],
+                        supplements: info['7d737'],
+                        autres_exemptions: info['7d741'],
+                        autres_exemptions_texte: info['7d742'],
+                        frais_obtention: info['7d746'],
+                        frais_de_deplacement: info['7d750'],
+                        design_lieu_travail: info['7d754']
                     }
                 }
             },
-            lieu2: {
-                contribuable: {
-                    localite: info['7d7'],
-                    periode: {
-                        du: info['7d7'],
-                        au: info['7d7']
+            plusieurs_lieux: {
+                lieu1: {
+                    contribuable: {
+                        localite: info['7d763'],
+                        periode: {
+                            du: info['7d765'],
+                            au: info['7d766']
+                        },
+                        frequence: {
+                            jour: info['7d769'],
+                            is_par_semaine: info['7d7'],
+                            is_par_mois: info['7d7']
+                        }
                     },
-                    frequence: {
-                        jour: info['7d7'],
-                        is_par_semaine: info['7d7'],
-                        is_par_mois: info['7d7']
+                    conjoint_partenaire: {
+                        localite: info['7d764'],
+                        periode: {
+                            du: info['7d767'],
+                            au: info['7d768']
+                        },
+                        frequence: {
+                            jour: info['7d770'],
+                            is_par_semaine: info['7d7'],
+                            is_par_mois: info['7d7']
+                        }
                     }
                 },
-                conjoint_partenaire: {
-                    localite: info['7d7'],
-                    periode: {
-                        du: info['7d7'],
-                        au: info['7d7']
+                lieu2: {
+                    contribuable: {
+                        localite: info['7d771'],
+                        periode: {
+                            du: info['7d773'],
+                            au: info['7d774']
+                        },
+                        frequence: {
+                            jour: info['7d777'],
+                            is_par_semaine: info['7d7'],
+                            is_par_mois: info['7d7']
+                        }
                     },
-                    frequence: {
-                        jour: info['7d7'],
-                        is_par_semaine: info['7d7'],
-                        is_par_mois: info['7d7']
+                    conjoint_partenaire: {
+                        localite: info['7d772'],
+                        periode: {
+                            du: info['7d775'],
+                            au: info['7d776']
+                        },
+                        frequence: {
+                            jour: info['7d778'],
+                            is_par_semaine: info['7d7'],
+                            is_par_mois: info['7d7']
+                        }
                     }
                 }
             }
