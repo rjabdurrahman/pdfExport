99b085aabfbd9d812e20f0bdf9c797a63c2230b1
aamamun8130 <md.abdullah8130@gmail.com>
2020-06-26 11:49:05 +0600

    Done: page 10 update done

--- Routes/api.js
+++ Routes/api.js
@@ -16,6 +16,7 @@ let page6 = require('../assets/page6');
 let page7 = require('../assets/page7');
 let page8 = require('../assets/page8');
 let page9 = require('../assets/page9');
+let page10 = require('../assets/page10');
 
 router.get('/', (req, res) => {
     res.send('API is live');
@@ -37,7 +38,7 @@ router.post('/infoupdate/:id', (req, res) => {
     let info = {};
     console.log(req.body)
     req.body.forEach(x => info[x.name] = x.value);
-    let data = { ...page1(info), ...page2(info), ...page3(info), ...page4(info), ...page5(info), ...page6(info), ...page7(info), ...page8(info), ...page9(info)};
+    let data = { ...page1(info), ...page2(info), ...page3(info), ...page4(info), ...page5(info), ...page6(info), ...page7(info), ...page8(info), ...page9(info), ...page10(info)};
     console.log(data)
     Client.findOneAndUpdate({ "_id": ObjectID(id) }, data, { upsert: true }, (err, result) => {
         if (err) res.send(err)
--- assets/page10.js
+++ assets/page10.js
@@ -3,108 +3,108 @@ module.exports = function (info) {
         location: {
             rev_non_exo: {
                 contribuable: {
-                    revenu_location: info['10d'],
-                    parts_revenu: info['10d'],
-                    revenu_concession: info['10d'],
-                    revenu_redevances: info['10d'],
-                    perte_location: info['10d'],
+                    revenu_location: info['10d1001'],
+                    parts_revenu: info['10d1005'],
+                    revenu_concession: info['10d1009'],
+                    revenu_redevances: info['10d1013'],
+                    perte_location: info['10d1017'],
                     f: {
-                        interets_debiteurs: info['10d'],
-                        part_non_deduite: info['10d'],
-                        total: info['10d']
+                        interets_debiteurs: info['10d1021'],
+                        part_non_deduite: info['10d1025'],
+                        total: info['10d1029']
                     }
                 },
                 conjoint_partenaire: {
-                    revenu_location: info['10d'],
-                    parts_revenu: info['10d'],
-                    revenu_concession: info['10d'],
-                    revenu_redevances: info['10d'],
-                    perte_location: info['10d'],
+                    revenu_location: info['10d1002'],
+                    parts_revenu: info['10d1006'],
+                    revenu_concession: info['10d1010'],
+                    revenu_redevances: info['10d1014'],
+                    perte_location: info['10d1018'],
                     f: {
-                        interets_debiteurs: info['10d'],
-                        part_non_deduite: info['10d'],
-                        total: info['10d']
+                        interets_debiteurs: info['10d1022'],
+                        part_non_deduite: info['10d1026'],
+                        total: info['10d1030']
                     }
                 },
-                totalCal: info['10d']
+                totalCal: info['10d1031']
             },
             rev_exo: {
                 contribuable: {
-                    revenu_location: info['10d'],
-                    parts_revenu: info['10d'],
-                    revenu_concession: info['10d'],
-                    revenu_redevances: info['10d'],
-                    perte_location: info['10d'],
+                    revenu_location: info['10d1003'],
+                    parts_revenu: info['10d1007'],
+                    revenu_concession: info['10d1011'],
+                    revenu_redevances: info['10d1015'],
+                    perte_location: info['10d1019'],
                     f: {
-                        interets_debiteurs: info['10d'],
-                        part_non_deduite: info['10d'],
-                        total: info['10d']
+                        interets_debiteurs: info['10d1023'],
+                        part_non_deduite: info['10d1027'],
+                        total: info['10d1032']
                     }
                 },
                 conjoint_partenaire: {
-                    revenu_location: info['10d'],
-                    parts_revenu: info['10d'],
-                    revenu_concession: info['10d'],
-                    revenu_redevances: info['10d'],
-                    perte_location: info['10d'],
+                    revenu_location: info['10d1004'],
+                    parts_revenu: info['10d1008'],
+                    revenu_concession: info['10d1012'],
+                    revenu_redevances: info['10d1016'],
+                    perte_location: info['10d1020'],
                     f: {
-                        interets_debiteurs: info['10d'],
-                        part_non_deduite: info['10d'],
-                        total: info['10d']
+                        interets_debiteurs: info['10d1024'],
+                        part_non_deduite: info['10d1028'],
+                        total: info['10d1033']
                     }
                 },
-                totalCal: info['10d']
+                totalCal: info['10d1034']
             },
             interets_debiteurs: {
                 table1: {
                     r1: {
-                        etablissement: info['10d'],
-                        relation: info['10d'],
-                        dette: info['10d']
+                        etablissement: info['10d1035'],
+                        relation: info['10d1036'],
+                        dette: info['10d1037']
                     },
                     r2: {
-                        etablissement: info['10d'],
-                        relation: info['10d'],
-                        dette: info['10d']
+                        etablissement: info['10d1040'],
+                        relation: info['10d1041'],
+                        dette: info['10d1042']
                     },
                     r3: {
-                        etablissement: info['10d'],
-                        relation: info['10d'],
-                        dette: info['10d']
+                        etablissement: info['10d1045'],
+                        relation: info['10d1046'],
+                        dette: info['10d1047']
                     }
                 },
 
                 table2: {
                     contribuable: {
-                        interets_debiteurs1: info['10d'],
-                        interets_debiteurs2: info['10d'],
-                        interets_debiteurs3: info['10d']
+                        interets_debiteurs1: info['10d1038'],
+                        interets_debiteurs2: info['10d1043'],
+                        interets_debiteurs3: info['10d1049']
                     },
                     conjoint_partenaire: {
-                        interets_debiteurs1: info['10d'],
-                        interets_debiteurs2: info['10d'],
-                        interets_debiteurs3: info['10d']
+                        interets_debiteurs1: info['10d1039'],
+                        interets_debiteurs2: info['10d1044'],
+                        interets_debiteurs3: info['10d1050']
                     }
                 },
 
                 habitationA: {
-                    habitation: info['10d'],
-                    numero: info['10d'],
-                    rue: info['10d'],
-                    date_occupation: info['10d'],
+                    habitation: info['10d1051'],
+                    numero: info['10d1053'],
+                    rue: info['10d1054'],
+                    date_occupation: info['10d1057'],
                     interets: {
-                        contribuable: info['10d'],
-                        conjoint_partenaire: info['10d']
+                        contribuable: info['10d1059'],
+                        conjoint_partenaire: info['10d1060']
                     }
                 },
                 habitationB: {
-                    habitation: info['10d'],
-                    numero: info['10d'],
-                    rue: info['10d'],
-                    date_occupation: info['10d'],
+                    habitation: info['10d1052'],
+                    numero: info['10d1055'],
+                    rue: info['10d1056'],
+                    date_occupation: info['10d1058'],
                     interets: {
-                        contribuable: info['10d'],
-                        conjoint_partenaire: info['10d']
+                        contribuable: info['10d1061'],
+                        conjoint_partenaire: info['10d1062']
                     }
                 }
             }
