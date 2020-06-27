54a8b6c7fe4a9d519a3bcc416624b32d6ab1bea7
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-06-27 13:58:16 +0600

    Added: Cookie login and 404 text'

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
 const express = require('express');
 const app = express();
 const path = require('path')
+const cookieParser = require('cookie-parser');
+const _ = require('lodash');
 const connectDB = require('./config');
+app.use(cookieParser());
 connectDB();
+const users = require('./assets/users.json');
 const apiRoute = require('./Routes/api');
 
 app.use(express.json());
-app.use('/api', apiRoute);
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.use(express.urlencoded({ extended: false }));
+
+app.post('/login', function (req, res) {
+    let user = _.find(users, req.body)
+    console.log(user)
+    if (user) {
+        res.cookie('user', user)
+        res.redirect('/')
+    }
+    else res.send('Login Failed');
 });
+app.get('/logout', (req, res) => {
+    res.clearCookie('user')
+    res.redirect('/')
+})
+
+app.use('/api', apiRoute);
+app.get('/', function (req, res) {
+    console.log(req.cookies)
+    app.use(express.static(path.join(__dirname, 'public')));
+    if (req.cookies.user) res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    else res.sendFile(path.join(__dirname, 'public/login', 'index.html'));
+});
+
+app.use(function (req, res) {
+    res.send('404 Page not Fount!');
+})
 
 const server = app.listen(process.env.PORT || 3000, () => console.log(`Listening on Port ${server.address().port}`));
