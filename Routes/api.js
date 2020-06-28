bc221bd751253e2657a56a776404f5840616a2a9
aamamun8130 <md.abdullah8130@gmail.com>
2020-06-28 13:01:12 +0600

    Done: page 19 update done

--- Routes/api.js
+++ Routes/api.js
@@ -24,6 +24,7 @@ let page14 = require('../assets/page14');
 let page15 = require('../assets/page15');
 let page16 = require('../assets/page16');
 let page17_18 = require('../assets/page17_18');
+let page19 = require('../assets/page19');
 
 router.get('/', (req, res) => {
     res.send('API is live');
@@ -44,7 +45,7 @@ router.post('/infoupdate/:id', (req, res) => {
     let id = req.params.id;
     let info = {};
     req.body.forEach(x => info[x.name] = x.value);
-    let data = { ...page1(info), ...page2(info), ...page3(info), ...page4(info), ...page5(info), ...page6(info), ...page7(info), ...page8(info), ...page9(info), ...page10(info), ...page11(info), ...page12(info), ...page13(info), ...page14(info), ...page15(info), ...page16(info), ...page17_18(info)};
+    let data = { ...page1(info), ...page2(info), ...page3(info), ...page4(info), ...page5(info), ...page6(info), ...page7(info), ...page8(info), ...page9(info), ...page10(info), ...page11(info), ...page12(info), ...page13(info), ...page14(info), ...page15(info), ...page16(info), ...page17_18(info), ...page19(info)};
     Client.findOneAndUpdate({ "_id": ObjectID(id) }, data, { upsert: true }, (err, result) => {
         if (err) res.send(err)
         else res.send(result)
--- assets/page19.js
+++ assets/page19.js
@@ -3,104 +3,104 @@ module.exports = function (info) {
         retenues_impot_diverses_demandes: {
             cat1: {
                 contribuable: {
-                    is_benefice_commercial: info['19d'],
-                    is_benef_agricole: info['19d'],
-                    is_benef_pro_liberale: info['19d'],
-                    montant_non_deduit: info['19d'],
-                    montant_deduit: info['19d']
+                    is_benefice_commercial: info['19d19'],
+                    is_benef_agricole: info['19d19'],
+                    is_benef_pro_liberale: info['19d19'],
+                    montant_non_deduit: info['19d1918'],
+                    montant_deduit: info['19d1920']
                 },
                 conjoint_partenaire: {
-                    is_benefice_commercial: info['19d'],
-                    is_benef_agricole: info['19d'],
-                    is_benef_pro_liberale: info['19d'],
-                    montant_non_deduit: info['19d'],
-                    montant_deduit: info['19d']
+                    is_benefice_commercial: info['19d19'],
+                    is_benef_agricole: info['19d19'],
+                    is_benef_pro_liberale: info['19d19'],
+                    montant_non_deduit: info['19d1919'],
+                    montant_deduit: info['19d1921']
                 }
             },
             cat2: {
-                is_report_91: info['19d'],
-                is_report_92: info['19d'],
+                is_report_91: info['19d19'],
+                is_report_92: info['19d19'],
                 contribuable: {
-                    report_91: info['19d'],
-                    report_92: info['19d']
+                    report_91: info['19d1923'],
+                    report_92: info['19d1927']
                 },
                 conjoint_partenaire: {
-                    report_91: info['19d'],
-                    report_92: info['19d']
+                    report_91: info['19d1924'],
+                    report_92: info['19d1928']
                 }
             },
             cat3: {
-                is_report_14: info['19d'],
+                is_report_14: info['19d19'],
                 contribuable: {
-                    report_14: info['19d']
+                    report_14: info['19d1931']
                 },
                 conjoint_partenaire: {
-                    report_14: info['19d']
+                    report_14: info['19d1932']
                 }
             },
             cat4: {
-                is_certificat: info['19d'],
+                is_certificat: info['19d19'],
                 contribuable: {
-                    abat_speciale_agricole: info['19d']
+                    abat_speciale_agricole: info['19d1935']
                 },
                 conjoint_partenaire: {
-                    abat_speciale_agricole: info['19d']
+                    abat_speciale_agricole: info['19d1936']
                 }
             },
             cat5: {
                 contribuable: {
-                    salaires: info['19d']
+                    salaires: info['19d1938']
                 },
                 conjoint_partenaire: {
-                    salaires: info['19d']
+                    salaires: info['19d1939']
                 }
             },
             cat6: {
                 contribuable: {
-                    pensions: info['19d']
+                    pensions: info['19d1940']
                 },
                 conjoint_partenaire: {
-                    pensions: info['19d']
+                    pensions: info['19d1941']
                 }
             },
             cat7: {
                 contribuable: {
-                    revenus_capitaux: info['19d']
+                    revenus_capitaux: info['19d1942']
                 },
                 conjoint_partenaire: {
-                    revenus_capitaux: info['19d']
+                    revenus_capitaux: info['19d1943']
                 }
             },
             cat8: {
                 contribuable: {
-                    impot_etranger_conv: info['19d']
+                    impot_etranger_conv: info['19d1945']
                 },
                 conjoint_partenaire: {
-                    impot_etranger_conv: info['19d']
+                    impot_etranger_conv: info['19d1946']
                 }
             },
             cat9: {
                 contribuable: {
-                    impot_etranger_annexe: info['19d']
+                    impot_etranger_annexe: info['19d1948']
                 },
                 conjoint_partenaire: {
-                    impot_etranger_annexe: info['19d']
+                    impot_etranger_annexe: info['19d1949']
                 }
             },
             cat10: {
                 contribuable: {
-                    retenue_impot: info['19d']
+                    retenue_impot: info['19d1951']
                 },
                 conjoint_partenaire: {
-                    retenue_impot: info['19d']
+                    retenue_impot: info['19d1952']
                 }
             },
             cat11: {
                 contribuable: {
-                    tantiemes: info['19d']
+                    tantiemes: info['19d1954']
                 },
                 conjoint_partenaire: {
-                    tantiemes: info['19d']
+                    tantiemes: info['19d1955']
                 }
             }
         }
