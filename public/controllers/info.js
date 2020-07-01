b0e5ec11e69133042081dcd40df30fa9b2c5402f
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-07-02 01:35:47 +0600

    Added: Loader on info click

--- public/controllers/info.js
+++ public/controllers/info.js
@@ -1,36 +1,44 @@
-function checkDone() {
-    $('.a01, .a02, .a03, .a04, .a05, .a06, .a07, .a08, .a09, .a10, .a11, .a12, .a13, .a14, .a15').each(function (i) {
-        let pageNo = this.id.replace('page', '');
-        let isDone = false;
-        let element = $(`.np${pageNo}`).children(1);
-        $(`#${this.id} input:text, #${this.id} input:checkbox`).each(function (j) {
-            if ((this.type == 'text' && this.value) || (this.type == 'checkbox' && this.checked)) {
-                isDone = true
-                element.addClass('done')
-            }
-        })
-        if (!isDone) element.removeClass('done');
+function checkDone () {
+  $(
+    '.a01, .a02, .a03, .a04, .a05, .a06, .a07, .a08, .a09, .a10, .a11, .a12, .a13, .a14, .a15'
+  ).each(function (i) {
+    let pageNo = this.id.replace('page', '')
+    let isDone = false
+    let element = $(`.np${pageNo}`).children(1)
+    $(`#${this.id} input:text, #${this.id} input:checkbox`).each(function (j) {
+      if (
+        (this.type == 'text' && this.value) ||
+        (this.type == 'checkbox' && this.checked)
+      ) {
+        isDone = true
+        element.addClass('done')
+      }
     })
+    if (!isDone) element.removeClass('done')
+  })
 }
 
-function radioCheck(info) {
-    if(info.etat_civil.status) $(`[value=${info.etat_civil.status}]`).attr('checked', true)
+function radioCheck (info) {
+  if (info.etat_civil.status)
+    $(`[value=${info.etat_civil.status}]`).attr('checked', true)
 }
 
 app.controller('InfoCtrl', function ($scope, $http) {
-    let clientId = location.href.split('id=')[1];
-    $scope.client = {};
-    $http({
-        method: 'GET',
-        url: `/api/client/${clientId}`
-    }).then(function (res) {
-        $('.load-overlay').hide();
-        $scope.client = res.data;
-        console.log(res.data)
-        radioCheck(res.data);
-        $scope.$applyAsync();
-        setTimeout(checkDone, 500);
-    }).catch(function (err) {
-        console.log(err);
+  let clientId = location.href.split('id=')[1]
+  $scope.client = {}
+  $http({
+    method: 'GET',
+    url: `/api/client/${clientId}`
+  })
+    .then(function (res) {
+      $('.load-overlay').hide();
+      $scope.client = res.data
+      console.log(res.data)
+      radioCheck(res.data)
+      $scope.$applyAsync()
+      setTimeout(checkDone, 500)
     })
-});
+    .catch(function (err) {
+      console.log(err)
+    })
+})
