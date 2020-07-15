7e9e229b2661bc045619f58879d89247b60182f5
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-07-15 13:27:30 +0600

    Update: comma validator in page5

--- public/controllers/info.js
+++ public/controllers/info.js
@@ -46,8 +46,8 @@ function radioCheck (info) {
 app.controller('InfoCtrl', function ($scope, $http) {
   $('input:text.date').datepicker({ dateFormat: 'dd/mm/yy' })
   $('.load-overlay').show()
-  $('.number').inputFilter(function (value) {
-    return /^\d*$/.test(value) // Allow digits only, using a RegExp
+  $('.num').inputFilter(function (value) {
+    return /^\d*,*\d*$/.test(value) // Allow digits only, using a RegExp
   })
   let clientId = location.href.split('id=')[1]
   $scope.client = {}
