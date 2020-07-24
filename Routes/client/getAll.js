cc6b9050536444821bdf77f2ad1e7a9154f83a58
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-07-24 15:44:13 +0600

    Fixed: List with only required fields

--- Routes/client/getAll.js
+++ Routes/client/getAll.js
@@ -1,7 +1,7 @@
 const Client = require('../../Models/Client');
 
 module.exports = (req, res) => {
-    Client.find((err, result) => {
+    Client.find({}).select('_id, signaletique.numero_de_dossier signaletique.contribuable').exec((err, result) => {
         if (err) res.send(err)
         else {
             res.send(result);
