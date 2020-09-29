befaedc29d0d58eba42cf7d8fd5d5383f911bd1e
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-09-29 23:02:30 +0600

    Done: Not logged redirect

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ connectDB()
 const userLogin = require('./Routes/user/login')
 const userLogOut = require('./Routes/user/logout')
 const apiRoute = require('./Routes/api')
+const { nextTick } = require('process')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.post('/login', userLogin)
 app.get('/logout', userLogOut)
 app.use('/api', apiRoute)
+app.use(/\/myapp\/(home|info)/, (req, res, next) => {
+  if (!req.cookies.user) {
+    res.redirect('/myapp/login')
+  }
+  else next()
+})
 app.use(express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
 const server = app.listen(process.env.PORT || 3000, () =>
   console.log(`Listening on Port:: ${server.address().port}`)
