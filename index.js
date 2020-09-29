de7ded9090c5968df0749b62ca7839d91603590c
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-09-29 21:18:18 +0600

    Added: /myapp home and login

--- index.js
+++ index.js
@@ -11,32 +11,13 @@ const apiRoute = require('./Routes/api')
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 app.post('/login', userLogin)
 app.get('/logout', userLogOut)
-
 app.use('/api', apiRoute)
 app.use(express.static('public'))
-app.get('/mylogin', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/login', 'index.html'))
-})
-app.get(
-  '/*',
-  function (req, res, next) {
-    if (!req.cookies.user) res.redirect('/mylogin');
-    else res.sendFile(path.join(__dirname, 'public', 'index.html'))
-    // return next();
-  }
-  // function (req, res, next) {
-  //     if (!req.cookies.user) {
-  //   res.sendFile(path.join(__dirname, 'public/login', 'index.html'));
-  //     }
-  //     else return next();
-  // },
-  // function (req, res, next) {
-  // }
-)
-
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'public', 'index.html'));
+// })
 const server = app.listen(process.env.PORT || 3000, () =>
   console.log(`Listening on Port:: ${server.address().port}`)
 )
