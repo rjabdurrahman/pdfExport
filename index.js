f18906d07ad4dfef32e6a1f2a92ab1b8d9649fa4
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-10-03 21:58:03 +0600

    Added: Pofile page on myapp

--- index.js
+++ index.js
@@ -25,6 +25,9 @@ app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'))
 })
+app.get('/myapp/profile', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'profile.html'))
+})
 const server = app.listen(process.env.PORT || 3000, () =>
   console.log(`Listening on Port:: ${server.address().port}`)
 )
