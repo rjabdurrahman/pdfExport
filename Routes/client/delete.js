122a995059142d0739e15d2b5ae1e30d75857650
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-11-03 02:50:56 +0600

    Added: Recycle funaction

--- Routes/client/delete.js
+++ Routes/client/delete.js
@@ -1,10 +1,34 @@
 const Client = require('../../Models/Client')
+const DeletedClient = require('../../Models/DeletedClient')
 let { ObjectID } = require('mongodb')
 
 module.exports = (req, res) => {
   let id = req.params.id
-  Client.remove({ _id: ObjectID(id) }, (err, result) => {
-    if (err) res.send({ err: err.message })
-    else res.send(result)
-  })
+  Client.findById(ObjectID(id), (err, client) => {
+    if (err) res.send(err)
+    else {
+      let refineClient = JSON.parse(JSON.stringify(client));
+      delete refineClient._id;
+      delete refineClient.__v;
+      delete refineClient.createdOn;
+      delete refineClient.modifiededOn;
+      //console.log(refineClient)
+      let deletedClient = new DeletedClient(refineClient)
+      deletedClient.save((err, data) => {
+        if (err) res.send({ err: err.message })
+        else 
+        {
+          Client.remove({ _id: ObjectID(id) }, (err, result) => {
+            if (err) res.send({ err: err.message })
+            else res.send(result)
+          });
+        }
+      })
+      
+      
+    }
+  });
+
+      
+
 }
