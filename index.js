b194ddcd83a0537c4e9cfc3f408f2b7ec1083b2c
rjabdurrahman <rj.abdurrahman@gmail.com>
2020-12-02 16:51:42 +0600

    Soved: FR domain

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ app.use(/\/myapp\/(home|info|profile|logout)*/, (req, res, next) => {
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   let domain = req.hostname;
-  // let domain = 'tax-benefits.lu';
+  // let domain = 'avantages-fiscaux.lu';
   if(domain == 'tax-benefits.lu')
     res.sendFile(path.join(__dirname, 'public', 'eng.html'))
-  else if(domain == 'avantages-fiscaux.lu') 
+  if(domain == 'avantages-fiscaux.lu') 
     res.sendFile(path.join(__dirname, 'public', 'fr.html'))
-  else
+  if(domain == 'y-e.lu')
     res.send('');
 })
 app.get('/myapp/*', (req, res) => {
