a0d95eda48a7262bf38a3e178c8281eb402d9e29
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-01-26 03:18:24 +0600

    Solved: register email problem

--- public/home.js
+++ public/home.js
@@ -1,55 +1,56 @@
 // Notifer
-function notify (msg, t) {
-    if (t == 1) {
-      $('#notification')
-        .html(
-          `<div class="contain">
+function notify(msg, t) {
+  if (t == 1) {
+    $('#notification')
+      .html(
+        `<div class="contain">
           <span class="material-icons">
               check_circle
           </span>
           <h6>${msg}</h6>
       </div>`
-        )
-        .removeClass('error')
-        .addClass('success')
-        .fadeIn(200)
-        .delay(1000)
-        .fadeOut(200)
-    }
-  
-    if (t == 2) {
-      $('#notification')
-        .html(
-          `<div class="contain">
+      )
+      .removeClass('error')
+      .addClass('success')
+      .fadeIn(200)
+      .delay(1000)
+      .fadeOut(200)
+  }
+
+  if (t == 2) {
+    $('#notification')
+      .html(
+        `<div class="contain">
           <span class="material-icons">
             highlight_off
           </span>
           <h6>${msg}</h6>
       </div>`
-        )
-        .removeClass('success')
-        .addClass('error')
-        .fadeIn(200)
-        .delay(1000)
-        .fadeOut(200)
-    }
+      )
+      .removeClass('success')
+      .addClass('error')
+      .fadeIn(200)
+      .delay(1000)
+      .fadeOut(200)
   }
+}
 
 function registerEmail(e) {
-    e.preventDefault();
-    let email = e.target['email'];
-    let lang = e.target['language'];
-    console.log(email.value, lang.value);
-    axios.post('api/user/subscribe', {email: email.value, language: lang.value})
+  e.preventDefault();
+  let email = e.target['email'];
+  let lang = e.target['language'];
+  console.log(email.value, lang.value);
+  axios.post('api/user/subscribe', { email: email.value, language: lang.value })
     .then(res => {
-        if(!res.data.err) {
-            notify('You are sucessfully Registered!', 1)
-        }
-        if(res.data.err.code == 11000) {
-            notify('Already Registered!', 2)
-        }
+      if (!res.data.err) {
+        notify('You are sucessfully Registered!', 1)
+      }
+      if (res.data.err && res.data.err.code == 11000) {
+        notify('Already Registered!', 2)
+      }
     })
     .catch(err => {
-        notify('Something Went Wrong', 2)
+      console.log(err)
+      notify('Something Went Wrong', 2)
     })
 }
