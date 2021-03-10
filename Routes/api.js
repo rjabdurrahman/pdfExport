1580fed08965e1d8abddcb9cb3c2ff867be843d9
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-11 01:10:35 +0600

    Added: New test group

--- Routes/api.js
+++ Routes/api.js
@@ -13,46 +13,52 @@ const gatherEmails = require('./userPanel/gatherEmails')
 const phoneContacts = require('./phoneContacts')
 
 router.get('/', (req, res) => {
-  res.send('API is live');
-})
-// Get Profile
+        res.send('API is live');
+    })
+    // Get Profile
 router.get('/profile', getProfile)
 router.post('/profile', updateProfile)
-// Adding new client.......
+    // Adding new client.......
 router.post('/addclient', addClient)
-//client info update
+    //client info update
 router.post('/infoupdate/:id', infoUpdate)
-// client delete
+    // client delete
 router.post('/delete/:id', deleteClient)
-// Single client find....
+    // Single client find....
 router.get('/client/:id', getOneClient)
-// All client find....
+    // All client find....
 router.get('/clients', getAllClient)
-// All recycled clients find....
+    // All recycled clients find....
 router.get('/recycled_clients', recycledClients)
-// Recovery recycled client...
+    // Recovery recycled client...
 router.get('/recover_client/:id', recycledClientRestore)
 router.get('/delete_forever/:id', deleteForever)
-// Genterate PDF
+    // Genterate PDF
 router.get('/pdf/:id', generatePdf)
 router.post('/user/subscribe', gatherEmails)
-// Phone Contacts
+    // Phone Contacts
 router.get('/phone_contacts', phoneContacts);
 
 router.get('/test_contacts', (req, res) => {
-  res.send(`<hunnyIPPhoneDirectory clearlight="true">
-  <Title>Phonelist</Title>
-  <Prompt>Prompt</Prompt>
-  <DirectoryEntry>
-  <Name>éàèüç</Name>
-  <Telephone>+8801757</Telephone>
-  <Telephone>+12412</Telephone>
-  </DirectoryEntry>
-  <DirectoryEntry>
-  <Name>Frédéric</Name>
-  <Telephone>5283283</Telephone>
-  </DirectoryEntry>
-  </hunnyIPPhoneDirectory>`)
+    res.send(`<YealinklPPhoneBook> 
+  <Title>Yealink</Title>
+  <Menu Name="Saler"> 
+  <Unit Name="Bella" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="8111"/> 
+  <Unit Name="Cindy" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="8116"/> 
+  <Unit Name="Miki" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phonel="81081"> 
+  <Unit Name="Lynn" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phonel="81097"> 
+  <Unit Name="Ian" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phonel="8107"/> 
+  <Unit Name="Sharon" default_photo="Resource:icon_friend_b.png" Phone3="" Phone2="" Phonel="8113"/> 
+  <Unit Name="Sanborn" default_photo="Resource:icon_blacklist_b.png" Phone3="" Phone2="" Phonel="8106"/> 
+  <Unit Name="lessica" default_photo="Config:photo.png" Phone3="" Phone2="" Phonel="8117"/> 
+  </Menu>
+  <Menu Name="Friend"> 
+  <Unit Name="Lily" default_photo="Config:photo.png" Phone3="" Phone2="" Phonel="3629"/> 
+  </Menu>
+  <Menu Name="Family">
+  <Unit Name="Lucy" default_photc.i="Config:photo.png" Phone3="" Phone2="" Phonel="3965"/> 
+  </Menu> 
+  </YealinkIPPhoneBook>`)
 })
 
 module.exports = router
