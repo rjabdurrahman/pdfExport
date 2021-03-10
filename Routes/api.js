7c8cbdb5023d32c577c38ca12d3a2b658f64a9d3
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-11 01:18:29 +0600

    Added: New test group

--- Routes/api.js
+++ Routes/api.js
@@ -43,20 +43,20 @@ router.get('/test_contacts', (req, res) => {
     res.send(`<YealinklPPhoneBook> 
   <Title>Yealink</Title>
   <Menu Name="Saler"> 
-  <Unit Name="Bella" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="8111"/> 
-  <Unit Name="Cindy" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phone1="8116"/> 
-  <Unit Name="Miki" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phonel="81081"> 
-  <Unit Name="Lynn" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phonel="81097"> 
-  <Unit Name="Ian" default_photo="Resource:icon_family_b.png" Phone3="" Phone2="" Phonel="8107"/> 
-  <Unit Name="Sharon" default_photo="Resource:icon_friend_b.png" Phone3="" Phone2="" Phonel="8113"/> 
-  <Unit Name="Sanborn" default_photo="Resource:icon_blacklist_b.png" Phone3="" Phone2="" Phonel="8106"/> 
-  <Unit Name="lessica" default_photo="Config:photo.png" Phone3="" Phone2="" Phonel="8117"/> 
+  <Unit Name="Bella" Phone1="8111"/> 
+  <Unit Name="Cindy" Phone1="8116"/> 
+  <Unit Name="Miki" Phonel="81081"> 
+  <Unit Name="Lynn" Phonel="81097"> 
+  <Unit Name="Ian" Phonel="8107"/> 
+  <Unit Name="Sharon" Phonel="8113"/> 
+  <Unit Name="Sanborn" Phonel="8106"/> 
+  <Unit Name="lessica" Phonel="8117"/> 
   </Menu>
   <Menu Name="Friend"> 
-  <Unit Name="Lily" default_photo="Config:photo.png" Phone3="" Phone2="" Phonel="3629"/> 
+  <Unit Name="Lily" Phonel="3629"/> 
   </Menu>
   <Menu Name="Family">
-  <Unit Name="Lucy" default_photc.i="Config:photo.png" Phone3="" Phone2="" Phonel="3965"/> 
+  <Unit Name="Lucy" Phonel="3965"/> 
   </Menu> 
   </YealinkIPPhoneBook>`)
 })
