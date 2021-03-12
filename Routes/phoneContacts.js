d7e11718c0d862446eb2b3bc1044c5e8cf699905
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-13 01:26:28 +0600

    Updated: Contacts Group

--- Routes/phoneContacts.js
+++ Routes/phoneContacts.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const qs = require('qs');
 
 
-module.exports = function (req, res) {
+module.exports = async function (req, res) {
   const APP_ID = 'bbe542cd-c9e2-41b3-a450-5cc568b9fb19';
   const APP_SECERET = '51hx5Qj~04sWG0L.8eX.6EBj-.5dLEo~-b';
   const TOKEN_ENDPOINT = 'https://login.microsoftonline.com/b346b961-5192-4ebb-a964-344dd8b205d0/oauth2/v2.0/token';
@@ -20,49 +20,21 @@ module.exports = function (req, res) {
   axios.defaults.headers.post['Content-Type'] =
     'application/x-www-form-urlencoded';
 
-  axios
-    .post(TOKEN_ENDPOINT, qs.stringify(postData))
-    .then(response => {
-      outlook.base.setApiEndpoint('https://graph.microsoft.com/v1.0');
-
-      let queryParams = {
-        '$select': 'GivenName,Surname,MobilePhone1,HomePhones,BusinessPhones',
-        '$orderby': 'CreatedDateTime desc',
-        '$top': 1000
-      };
-
-      let userInfo = {
-        email: 'info@y-e.lu'
-      };
-      outlook.contacts.getContacts({ token: response.data.access_token, odataParams: queryParams, user: userInfo },
-        function (error, result) {
-          if (error) {
-            res.send(error);
-          }
-          else if (result) {
-            let phoneStr = `<hunnyIPPhoneDirectory clearlight="true">
-            <Title>Phonelist</Title>
-            <Prompt>Prompt</Prompt>`;
-            result.value.forEach(function (contact) {
-              phoneStr += `<DirectoryEntry>
-              <Name>${contact.surname ? contact.surname + ' ' : ''}${contact.givenName ? contact.givenName : ''}</Name>`;
-              if (contact.mobilePhone)
-                phoneStr += `<Telephone>${contact.mobilePhone.replace(/\s+/g, '')}</Telephone>
-                `;
-              if (contact.homePhones.length)
-                phoneStr += `<Telephone>${contact.homePhones[0].replace(/\s+/g, '')}</Telephone>
-                `;
-              if (contact.businessPhones.length)
-                phoneStr += `<Telephone>${contact.businessPhones[0].replace(/\s+/g, '')}</Telephone>
-                `;
-              phoneStr += `</DirectoryEntry>
-              `;
-            });
-            res.send(phoneStr + `</hunnyIPPhoneDirectory>`);
-          }
-        });
-    })
-    .catch(error => {
-      console.log(error.message);
-    });
+  try {
+    let { data } = await axios.post(TOKEN_ENDPOINT, qs.stringify(postData));
+    let { access_token } = data;
+    outlook.base.setApiEndpoint('https://graph.microsoft.com/v1.0');
+    let response = await Promise.all([
+      axios.get(`https://graph.microsoft.com/v1.0/users('info@y-e.lu')/contacts?$select=GivenName,Surname,categories,MobilePhone1,HomePhones,BusinessPhones&$top=1000`, { headers: { "Authorization": access_token } }),
+      axios.get(`https://graph.microsoft.com/v1.0/users('info@y-e.lu')/contacts?$select=GivenName,Surname,categories,MobilePhone1,HomePhones,BusinessPhones&$top=1000&$skip=1000`, { headers: { "Authorization": access_token } })
+    ]);
+    let allContacts = [...response[0].data.value, ...response[1].data.value]
+      .reduce(function (rv, x) {
+        (rv[x['categories']] = rv[x['categories']] || []).push(x);
+        return rv;
+      }, {});
+    res.send(allContacts);
+  } catch (err) {
+    res.send('err' + err);
+  }
 }
