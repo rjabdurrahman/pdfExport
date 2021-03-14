e303f5d1493f06be0b22f339508dad33d2d3116d
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-15 01:31:57 +0600

    Fixed: Proxy to angular v2

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'client';
+  constructor(private http: HttpClient) { }
+  getContacts() {
+    this.http.get('/api/phone_contacts?pass=vCwm7MpeYPrlJfOnPCe9').subscribe(x => console.log(x));
+  }
 }
