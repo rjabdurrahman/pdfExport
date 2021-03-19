ee8f9e3dc82c812369080c7f54c39f36a0f297b2
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-20 00:49:38 +0600

    Done: Transfer

--- Routes/client/transfer.js
+++ Routes/client/transfer.js
@@ -1,10 +1,20 @@
+const Client = require('../../Models/Client');
+
 function transferClient({ body }, res) {
     let clientId = body.client._id;
     let selectedYear = body.selectedYear;
     let targetYear = body.targetYear;
-    console.log(clientId);
-    console.log(selectedYear, targetYear);
-    res.send('done');
+    Client.findById(clientId, (err, client) => {
+        if (err) res.send(err)
+        else {
+            Client.findByIdAndUpdate(clientId, {
+                ['y' + targetYear]: client['y' + selectedYear]
+            }, {new: true}, (err, client) => {
+                if(err) res.send(err);
+                else res.send(client);
+            });
+        }
+    });
 }
 
 module.exports = transferClient;
