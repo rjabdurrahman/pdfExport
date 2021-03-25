0a290b213d6ea813ec7d7f5c28abea7036611494
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-25 11:41:56 +0600

    Updated: 2019 2020 Pdf generate process

--- Routes/generatePdf.js
+++ Routes/generatePdf.js
@@ -7,6 +7,11 @@ let fdfText = require('../pdf/fdfText');
 
 module.exports = (req, res) => {
     let id = req.params.id;
+    let year = req.params.year;
+    let pdfFile = '';
+    if(year == 2019) pdfFile = '2019_editable.pdf';
+    else if(year == 2020) pdfFile = '2020.pdf';
+    console.log('Year', year);
     Client.findById(id, (err, client) => {
         if (err) res.send(err)
         else {
@@ -20,7 +25,7 @@ module.exports = (req, res) => {
                 else {
                     let prefix = '';
                     // if(process.platform != 'win32') prefix = 'chmod u+x /app/vendor/pdftk/bin/pdftk && ';
-                    exec(`${prefix}pdftk ./pdf/2019_editable.pdf fill_form ./pdf/data_bind.fdf output ./pdf/client_files/abc.pdf`, (error, stdout, stderr) => {
+                    exec(`${prefix}pdftk ./pdf/${pdfFile} fill_form ./pdf/data_bind.fdf output ./pdf/client_files/abc.pdf`, (error, stdout, stderr) => {
                         if (error) {
                             res.send(`error: ${error.message}`);
                             return;
