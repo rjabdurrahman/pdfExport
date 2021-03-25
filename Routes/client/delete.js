7bfbbce6bcb15ff6e2d9f2f6c095963ea3f5545c
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-26 02:14:31 +0600

    updated: PDF

--- Routes/client/delete.js
+++ Routes/client/delete.js
@@ -2,33 +2,27 @@ const Client = require('../../Models/Client')
 const DeletedClient = require('../../Models/DeletedClient')
 let { ObjectID } = require('mongodb')
 
-module.exports = (req, res) => {
-  let id = req.params.id
-  Client.findById(ObjectID(id), (err, client) => {
-    if (err) res.send(err)
-    else {
-      let refineClient = JSON.parse(JSON.stringify(client));
-      delete refineClient._id;
-      delete refineClient.__v;
-      delete refineClient.createdOn;
-      delete refineClient.modifiededOn;
-      //console.log(refineClient)
-      let deletedClient = new DeletedClient(refineClient)
-      deletedClient.save((err, data) => {
-        if (err) res.send({ err: err.message })
-        else 
-        {
-          Client.remove({ _id: ObjectID(id) }, (err, result) => {
-            if (err) res.send({ err: err.message })
-            else res.send(result)
-          });
-        }
-      })
-      
-      
-    }
-  });
+module.exports = async (req, res) => {
+  let id = req.params.id;
+  let year = req.params.year;
+  try {
+    let client = await Client.findById(ObjectID(id)).lean().exec();
+    delete client._id;
+    delete client.__v;
+    delete client.createdOn;
+    delete client.modifiededOn;
+    let deletedClient = new DeletedClient(client)
+    deletedClient.save((err, data) => {
+      if (err) res.send({ err: err.message })
+      else {
+        Client.remove({ _id: ObjectID(id) }, (err, result) => {
+          if (err) res.send({ err: err.message })
+          else res.send(result)
+        });
+      }
+    })
 
-      
-
+  } catch (err) {
+    res.send(err);
+  }
 }
