e9d71c0bfb943824b41517b23bd727a71c42ee53
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-26 02:02:26 +0600

    updated: transfer

--- Routes/client/transfer.js
+++ Routes/client/transfer.js
@@ -1,21 +1,21 @@
 const Client = require('../../Models/Client');
 
-function transferClient({ body }, res) {
-    let clientId = body.client._id;
-    let selectedYear = body.selectedYear;
-    let targetYear = body.targetYear;
-    Client.findById(clientId, (err, client) => {
-        if (err) res.send(err)
-        else {
-            let { signaletique, enfants, etat_civil } = client['y' + selectedYear];
-            Client.findByIdAndUpdate(clientId, {
-                ['y' + targetYear]: { signaletique, enfants, etat_civil }
-            }, { new: true }, (err, client) => {
-                if (err) res.send(err);
-                else res.send(client);
-            });
-        }
-    });
+async function transferClient({ body }, res) {
+    try {
+        let clientId = body.client._id;
+        let selectedYear = body.selectedYear;
+        let targetYear = body.targetYear;
+        let client = await Client.findById(clientId).lean().exec();
+        let { signaletique, enfants, etat_civil } = client['y' + selectedYear];
+        Client.findByIdAndUpdate(clientId, {
+            ['y' + targetYear]: { signaletique, enfants, etat_civil }
+        }, { new: true }, (err, client) => {
+            if (err) res.send(err);
+            else res.send(client);
+        });
+    } catch (err) {
+        res.send(err);
+    }
 }
 
 module.exports = transferClient;
