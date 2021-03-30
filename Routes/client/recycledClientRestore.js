4bc20942cb28591673dc93709b94fccaa8189703
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-03-30 13:08:09 +0600

    Fixed: Restore function for yearly

--- Routes/client/recycledClientRestore.js
+++ Routes/client/recycledClientRestore.js
@@ -1,25 +1,18 @@
 const Client = require('../../Models/Client')
 const DeletedClient = require('../../Models/DeletedClient')
-let { ObjectID } = require('mongodb')
 
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   let id = req.params.id
-  DeletedClient.findById(ObjectID(id), (err, client) => {
-    if (err) res.send(err)
+  let client = await DeletedClient.findById(id).lean().exec();
+  let oldId = client.oldId;
+  delete client._id;
+  delete client.__v;
+  delete client.oldId;
+  Client.findByIdAndUpdate(oldId, client, (err, data) => {
+    if (err) res.send({ err: err.message })
     else {
-      let refineClient = JSON.parse(JSON.stringify(client))
-      delete refineClient._id
-      delete refineClient.__v
-      delete refineClient.createdOn
-      delete refineClient.modifiededOn
-      let recovedClient = new Client(refineClient)
-      // recovedClient.save((err, data) => {
-      //   if (err) res.send({ err: err.message })
-      //   else {
-      //     res.send({ success: 'Restored Client Successfully' })
-      //     DeletedClient.remove({ _id: ObjectID(id) }, (err, result) => {})
-      //   }
-      // })
+      res.send({ success: 'Restored Client Successfully' })
+      DeletedClient.findByIdAndDelete(id, (err, result) => { })
     }
-  })
+  });
 }
