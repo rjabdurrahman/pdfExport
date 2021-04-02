fb971c10d746c094d91c6e826248ba6455e0ae2f
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-04-02 23:14:44 +0600

    Fixed: All clients getting null data

--- Routes/client/getAll.js
+++ Routes/client/getAll.js
@@ -3,7 +3,7 @@ const Client = require('../../Models/Client');
 module.exports = async ({ params }, res) => {
     try {
         let result = await Client.find(
-            { ['y' + params.year]: { $exists: true } },
+            {},
             {
                 ["y2019" + ".signaletique.numero_de_dossier"]: 1,
                 ["y2019" + ".signaletique.contribuable.nom"]: 1,
@@ -16,8 +16,7 @@ module.exports = async ({ params }, res) => {
                 ["y2020" + ".signaletique.contribuable.telephone"]: 1,
                 ["y2020" + ".signaletique.contribuable.courriel"]: 1
             }
-        )
-            .lean().exec();
+        ).exec();
         res.send(result);
     } catch (err) {
         res.send(err);
