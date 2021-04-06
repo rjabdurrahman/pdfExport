46c683fa2008875be08efcbd938e2ed385628da4
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-04-07 00:13:07 +0600

    V2@Added: Add Client Modal view

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { FormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { CreateNewClientComponent } from './create-new-client/create-new-client.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    CreateNewClientComponent
   ],
   imports: [
     BrowserModule,
---
+++ client/src/app/create-new-client/create-new-client.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CreateNewClientComponent } from './create-new-client.component';
+
+describe('CreateNewClientComponent', () => {
+  let component: CreateNewClientComponent;
+  let fixture: ComponentFixture<CreateNewClientComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ CreateNewClientComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateNewClientComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
