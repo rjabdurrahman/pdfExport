71f9ff302128f522eefb13cc78a9f5cdfd6d518c
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-04-24 16:17:24 +0600

    Updated: phantom border color

--- public/myapp/controllers/info.js
+++ public/myapp/controllers/info.js
@@ -114,7 +114,10 @@ app.controller('InfoCtrl', function ($rootScope, $scope, $http) {
       afterDataLoaded($scope.client);
       $(".form-input input:not(:disabled)").addClass("temp-disable");
       $(".form-input input:not(:disabled)").prop("disabled", true);
-      setTimeout(() => $('a.done span').css('color', '#cf4044'), 500);
+      setTimeout(() => {
+        $('a.done span').css('color', '#cf4044')
+        $('a.done').css('border-color', '#cf4044')
+      }, 500);
       $('#submitInfoBtn').css('visibility', 'hidden');
     } else {
       $scope.client = $scope.data20;
@@ -122,7 +125,10 @@ app.controller('InfoCtrl', function ($rootScope, $scope, $http) {
       $(".form-input input.temp-disable").prop("disabled", false);
       $(".form-input input.temp-disable").removeClass("temp-disable");
       $('left-nav a.done span').css('color', '#25c1a0');
-      setTimeout(() => $('a.done span').css('color', '#25c1a0'), 500);
+      setTimeout(() => {
+        $('a.done span').css('color', '#25c1a0');
+        $('a.done').css('border-color', '#25c1a0');
+      }, 500);
       $('#submitInfoBtn').css('visibility', 'visible');
     }
     $scope.$applyAsync();
