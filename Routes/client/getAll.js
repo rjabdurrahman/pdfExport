587e41128c51de84f6dfa3fe592bd12333c3f093
rjabdurrahman <rj.abdurrahman@gmail.com>
2021-04-24 17:00:07 +0600

    Updated: 2nd percent cal

--- Routes/client/getAll.js
+++ Routes/client/getAll.js
@@ -11,7 +11,8 @@ module.exports = async ({ params }, res) => {
                 ["y2019" + ".signaletique.contribuable.telephone"]: 1,
                 ["y2019" + ".signaletique.contribuable.courriel"]: 1,
                 ["y2019" + ".signaletique.conjoint_partenaire.prenom"]: 1,
-                ["y2019" + ".depenses_speciales15.prevoyance_vieillesse_t2.contribuable.plafond_3200_total"]: 1,
+                ["y2019" + ".depenses_speciales15.prevoyance_vieillesse_t2.contribuable.plafond_3200"]: 1,
+                ["y2019" + ".depenses_speciales15.prevoyance_vieillesse_t2.conjoint_partenaire.plafond_3200"]: 1,
                 ["y2019" + ".depenses_speciales14.plafond_672"]: 1,
                 ["y2019" + ".depenses_speciales14.montant_moins_eleve"]: 1,
                 ["y2020" + ".signaletique.numero_de_dossier"]: 1,
@@ -22,7 +23,8 @@ module.exports = async ({ params }, res) => {
                 ["y2020" + ".depenses_speciales14.plafond_672"]: 1,
                 ["y2020" + ".depenses_speciales14.montant_moins_eleve"]: 1,
                 ["y2020" + ".signaletique.conjoint_partenaire.prenom"]: 1,
-                ["y2020" + ".depenses_speciales15.prevoyance_vieillesse_t2.contribuable.plafond_3200_total"]: 1
+                ["y2020" + ".depenses_speciales15.prevoyance_vieillesse_t2.contribuable.plafond_3200"]: 1,
+                ["y2020" + ".depenses_speciales15.prevoyance_vieillesse_t2.conjoint_partenaire.plafond_3200"]: 1,
             }
         ).exec();
         res.send(result);
--- public/myapp/controllers/info.js
+++ public/myapp/controllers/info.js
@@ -75,10 +75,15 @@ app.controller('ClientsListControler', function ($scope, $rootScope, $http) {
     }
   }
 
-  $scope.calculateParcent2 = function(conj, val) {
-    val = Number(val.replace('.', '').replace(',', '.'));
-    // console.log(conj, val);
-    return Math.round(val/3200 * 100);
+  $scope.calculateParcent2 = function(conj, v1, v2) {
+    let ground = 3200;
+    v1 = Number(v1.replace('.', '').replace(',', '.'));
+    v2 = Number(v2.replace('.', '').replace(',', '.'));
+    if(conj || v1 > 0) {
+      ground *= 2;
+    }
+    // console.log(conj, v1, v2);
+    return Math.round((v1 + v2)/ground * 100);
   }
 })
 
