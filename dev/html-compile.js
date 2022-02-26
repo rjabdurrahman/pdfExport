f35a6ea3c503436c9585667296a2fb03febdb0d9
rjabdurrahman <rj.abdurrahman@gmail.com>
2022-02-26 13:56:02 +0600

    Added: Page 17 and 18 new

--- dev/html-compile.js
+++ dev/html-compile.js
@@ -3,14 +3,14 @@ const config = require('./html-config.json');
 let importsRegx = /{{<.+}}$/gm
 let html = '';
 let dir = config.rootDir;
-fs.readFile(dir + 'info20.hbs', 'UTF-8', (err, text) => {
+fs.readFile(dir + 'info21.hbs', 'UTF-8', (err, text) => {
     if (err) console.log(err.message)
     html = text
     for (e of text.match(importsRegx)) {
         let text = fs.readFileSync(dir + e.replace('{{<', '').replace('}}', ''), { encoding: 'utf8' });
         html = html.replace(e, text);
     }
-    fs.writeFile(config.outDir + 'info20.html', html, err => {
+    fs.writeFile(config.outDir + 'info21.html', html, err => {
         if (err) console.log(err.message)
         else console.log('HTML Compiled');
     });
