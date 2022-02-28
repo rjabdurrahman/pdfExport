223c51255d9386de6654e882aa1bba4838afb482
rjabdurrahman <rj.abdurrahman@gmail.com>
2022-02-28 14:44:38 +0600

    Updated: 2021 users list view

--- Routes/client/getAll.js
+++ Routes/client/getAll.js
@@ -28,7 +28,19 @@ module.exports = async ({ params }, res) => {
                 ["y2020" + ".depenses_speciales14.plafond_672"]: 1,
                 ["y2020" + ".depenses_speciales15.prevoyance_vieillesse_t2.contribuable.plafond_3200"]: 1,
                 ["y2020" + ".depenses_speciales15.prevoyance_vieillesse_t2.conjoint_partenaire.plafond_3200"]: 1,
-                ["y2020" + ".depenses_speciales14.montant_moins_eleve"]: 1
+                ["y2020" + ".depenses_speciales14.montant_moins_eleve"]: 1,
+                ["y2021" + ".signaletique.numero_de_dossier"]: 1,
+                ["y2021" + ".signaletique.contribuable.nom"]: 1,
+                ["y2021" + ".signaletique.contribuable.prenom"]: 1,
+                ["y2021" + ".signaletique.contribuable.telephone"]: 1,
+                ["y2021" + ".signaletique.contribuable.courriel"]: 1,
+                ["y2021" + ".signaletique.contribuable.date_de_naissance"]: 1,
+                ["y2021" + ".signaletique.conjoint_partenaire.date_de_naissance"]: 1,
+                ["y2021" + ".signaletique.conjoint_partenaire.nom"]: 1,
+                ["y2021" + ".depenses_speciales14.plafond_672"]: 1,
+                ["y2021" + ".depenses_speciales15.prevoyance_vieillesse_t2.contribuable.plafond_3200"]: 1,
+                ["y2021" + ".depenses_speciales15.prevoyance_vieillesse_t2.conjoint_partenaire.plafond_3200"]: 1,
+                ["y2021" + ".depenses_speciales14.montant_moins_eleve"]: 1
             }
         ).exec();
         res.send(result);
