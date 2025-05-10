3409a93a7308eb2484baaedabbdba5064af1aba0
Abdur Rahman <md.rahman0296@gmail.com>
2025-05-10 18:31:33 +0600

    Updated: info24.hbs

--- dev/html-compile.js
+++ dev/html-compile.js
@@ -3,14 +3,16 @@ const config = require('./html-config.json');
 let importsRegx = /{{<.+}}$/gm
 let html = '';
 let dir = config.rootDir;
-fs.readFile(dir + 'info23.hbs', 'UTF-8', (err, text) => {
+// New Year Modify
+fs.readFile(dir + 'info24.hbs', 'UTF-8', (err, text) => {
     if (err) console.log(err.message)
     html = text
     for (e of text.match(importsRegx)) {
         let text = fs.readFileSync(dir + e.replace('{{<', '').replace('}}', ''), { encoding: 'utf8' });
         html = html.replace(e, text);
     }
-    fs.writeFile(config.outDir + 'info23.html', html, err => {
+    // New Year Modify
+    fs.writeFile(config.outDir + 'info24.html', html, err => {
         if (err) console.log(err.message)
         else console.log('HTML Compiled');
     });
